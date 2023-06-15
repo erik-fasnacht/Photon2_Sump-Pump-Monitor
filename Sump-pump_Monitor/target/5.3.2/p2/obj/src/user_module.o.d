7bd30108aaf71ffb4a1e0e43d5cbdb43f3558a25
erik-fasnacht <erik.fasnacht@particle.io>
2023-06-15 09:20:15 -0600

    removed unnecessary files

--- Sump-pump_Monitor/target/5.3.2/p2/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-/Users/Erik-Home/Documents/GitHub/Photon2-Project_Erik/Sump-pump_Monitor/target/5.3.2/p2//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
